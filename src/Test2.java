97e30ba27e4ff5e607a0fd0a2d738f3df19f8de4
Levin0200 <namseongcheon905@gamil.com>
2026-03-11 03:22:57 +0900

    간단한 메서드

--- src/Test2.java
+++ src/Test2.java
@@ -1,3 +1,6 @@
 public class Test2 {
     //team 2
+    public int add(int a, int b){
+        return a+b;
+    }
 }
--- src/Test3.java
+++ src/Test3.java
@@ -1,3 +1,6 @@
 public class Test3 {
     //team3
+    public void hello(){
+        System.out.println("테스트 3이랍니다");
+    }
 }
